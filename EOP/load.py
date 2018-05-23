b38f598e618eae5555a3297df3b3c36edf3fc853
cmdrlucienn <cmdrlucienn@gmail.com>
2018-05-22 21:22:16 -0400

    added more template functions to load.py

--- EOP/load.py
+++ EOP/load.py
@@ -1,12 +1,28 @@
+import sys
+import ttk
+import Tkinter as tk
+import requests
+
+from config import applongname, appversion
+import myNotebook as nb
+from config import config
+
+this = sys.modules[__name__]
+
 def plugin_start():
-	"""
-	Load this plugin into EDMC
-	"""
-	print "I am loaded!"
+	print "Loading EOProgression..."
 	return "Test"
-  
+
+def plugin_prefs(parent):
+	frame = nb.Frame(parent)
+	frame.columnconfigure(1, weight=1)
+	
+	plugin_label = nb.Label(frame, text="EOProgression Configuration")
+	plugin_label.grid(padx=10, row=8, sticky=tk.W)
+	
+	return frame
+
+def prefs_changed():
+	
 def plugin_stop():
-	"""
-	EDMC is closing
-	"""
-	print "Farewell cruel world!"
+	print "EOProgression Closing"
