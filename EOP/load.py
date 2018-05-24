b7f7529cc2eda6bdfcd848ab1f9a0ad0e511587e
cmdrlucienn <cmdrlucienn@gmail.com>
2018-05-23 21:41:29 -0400

    start of external window. maybe. also a button.

--- EOP/load.py
+++ EOP/load.py
@@ -13,6 +13,34 @@ def plugin_start():
 	print "Loading EOProgression..."
 	return "Test"
 
+def plugin_app(parent):
+	this.parent = parent
+	button = tk.Button(parent, text="EOProgression", command=plugin_window)
+
+def plugin_window(parent):
+	this.window = tk.TopLevel()
+	this.window.title("EOProgression")
+	this.window.resizable(width=False, height=False)
+	
+	tab = nb.Notebook(this.window)
+	
+	combat_page = nb.Frame(tab) # Combat tab
+	
+	trader_page = nb.Frame(tab) # Trade tab
+	
+	explor_page = nb.Frame(tab) # Explorer tab
+	
+	tab.add(combat_page, text='Combat')
+	tab.add(trader_page, text='Trade')
+	tab.add(explor_page, text='Exploration')
+	
+	tab.pack(expand=1, fill="both")
+	
+	this.window.mainloop()
+	
+def plugin_stop():
+	print "EOProgression Closing"
+	
 def plugin_prefs(parent):
 	frame = nb.Frame(parent)
 	frame.columnconfigure(1, weight=1)
@@ -23,6 +51,3 @@ def plugin_prefs(parent):
 	return frame
 
 def prefs_changed():
-	
-def plugin_stop():
-	print "EOProgression Closing"
